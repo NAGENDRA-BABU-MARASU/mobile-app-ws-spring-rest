07661dad5f996e325b6882d2005c8ce90b763cfa
Nagendra <nagendrababu.marasu@gmail.com>
2024-04-22 10:32:16 +0530

    added requestParameters for users resource

--- src/main/java/com/learnings/app/ws/ui/controller/UserController.java
+++ src/main/java/com/learnings/app/ws/ui/controller/UserController.java
@@ -7,22 +7,29 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping(path = "/{userId}")
-    public String getUser(@PathVariable String userId){
+    public String getUser(@PathVariable String userId) {
         return "getUser was called with userId=" + userId;
     }
 
+    @GetMapping()
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+                           @RequestParam(value = "limit", defaultValue = "50") int limit,
+                           @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+        return "getUsers was called with page=" + page + " & limit=" + limit + " & sort=" + sort;
+    }
+
     @PostMapping
-    public String createUser(){
+    public String createUser() {
         return "createUser was called";
     }
 
     @PutMapping
-    public String updateUser(){
+    public String updateUser() {
         return "updateUser was called";
     }
 
     @DeleteMapping
-    public String deleteUser(){
+    public String deleteUser() {
         return "deleteUser was called";
     }
 }
