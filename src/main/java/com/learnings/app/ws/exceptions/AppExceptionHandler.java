25da97308aa1784c8a533e0cddcd299de4e4f18f
Nagendra <nagendrababu.marasu@gmail.com>
2024-04-23 10:42:44 +0530

    implemented custom exception error message object to return when there is an exception

--- src/main/java/com/learnings/app/ws/exceptions/AppExceptionHandler.java
+++ src/main/java/com/learnings/app/ws/exceptions/AppExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.learnings.app.ws.exceptions;
 
+import com.learnings.app.ws.ui.model.response.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +9,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class AppExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request) {
-        return new ResponseEntity<>(ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if(errorMessageDescription == null) errorMessageDescription = ex.toString();
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
